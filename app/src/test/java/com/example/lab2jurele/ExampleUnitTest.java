61c08c5f3cfea33c2b8e1f63c6f38ead7681ddcf
sjurele <steponas.j@gmail.com>
2021-05-28 16:47:29 +0300

    Atkiktas testavimas su ivairiais variantais. Patikrinti testo rezultatai visos klases "class ExampleUnitTest"

--- app/src/test/java/com/example/lab2jurele/ExampleUnitTest.java
+++ app/src/test/java/com/example/lab2jurele/ExampleUnitTest.java
@@ -10,8 +10,115 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    @Test(expected = NullPointerException.class)
+    public void WordsCountWhenNull(){
+        Calculator.getNumbersCount(null); }
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void TextCountWords(){
+        final String givenString = "Zalia balta";
+        final int expectedResult = 2;
+        final int actualResult = Calculator.getWordsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
     }
+
+    @Test
+    public void TextCountNumbers(){
+        final String givenString = "Zalia balta";
+        final int expectedResult = 0;
+        final int actualResult = Calculator.getNumbersCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void TextCountChars(){
+        final String givenString = "Zalia balta";
+        final int expectedResult = 11;
+        final int actualResult = Calculator.getCharsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void TextCountEmptyWords(){
+        final String givenString = "           ";
+        final int expectedResult = 0;
+        final int actualResult = Calculator.getWordsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void TextCountEmptyChars(){
+        final String givenString = "              ";
+        final int expectedResult = 14;
+        final int actualResult = Calculator.getCharsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void TextCountSymbolsNumbers(){
+        final String givenString = "%$$@%#$^%#$%";
+        final int expectedResult = 0;
+        final int actualResult = Calculator.getNumbersCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void TextCountSymbolsChars(){
+        final String givenString = "%$$@%#$^%#$%";
+        final int expectedResult = 12;
+        final int actualResult = Calculator.getCharsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void TextCountNumbersCheck(){
+        final String givenString = "1234****56789";
+        final int expectedResult = 9;
+        final int actualResult = Calculator.getNumbersCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void TextCountCharsCheck(){
+        final String givenString = "1234****56789";
+        final int expectedResult = 13;
+        final int actualResult = Calculator.getCharsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void TextCountCharsMix(){
+        final String givenString = "* 4 * / / 0 0 ____ 999 999 ___++ )___";
+        final int expectedResult = 37;
+        final int actualResult = Calculator.getCharsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void TextCountNumbersMix(){
+        final String givenString = "* 4 * / / 0 0 ____ 999 999 ___++ )___";
+        final int expectedResult = 9;
+        final int actualResult = Calculator.getNumbersCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void WordsCountNone(){
+        final String givenString = "";
+        final int expectedResult = 0;
+        final int actualResult = Calculator.getWordsCount(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
 }
